be5cb28f4243b7f5a83d38d90f88ae2e575cc431
Jake Zhao <jiaxi_1@hotmail.com>
2026-08-31 18:07:50 -0400

    Build all q-Fibonomial proof modules as Lake roots

--- problems/math-0002-q-fibonomial-n4/formal/lakefile.lean
+++ problems/math-0002-q-fibonomial-n4/formal/lakefile.lean
@@ -7,4 +7,5 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.33.1"
 
 @[default_target]
-lean_lib QFibonomial4
+lean_lib QFibonomial4 where
+  roots := #[`QFibonomial4, `QFibonomial4All, `QFibonomial4Series]
