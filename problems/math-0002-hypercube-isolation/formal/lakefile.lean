050da69becf979b893b62c89c6b32a8aee627edf
Jake Zhao <jiaxi_1@hotmail.com>
2026-09-02 18:19:50 -0400

    math-0002: add Mathlib for structural formalization

--- problems/math-0002-hypercube-isolation/formal/lakefile.lean
+++ problems/math-0002-hypercube-isolation/formal/lakefile.lean
@@ -3,5 +3,8 @@ open Lake DSL
 
 package HypercubeIsolation where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.1"
+
 @[default_target]
 lean_lib HypercubeIsolation
