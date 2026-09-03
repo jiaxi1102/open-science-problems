b705372d8d1e7045184227e18ac11aafd555e15f
Jake Zhao <jiaxi_1@hotmail.com>
2026-09-02 20:33:44 -0400

    math-0002: include puncturing rigidity module in default Lean target

--- problems/math-0002-hypercube-isolation/formal/lakefile.lean
+++ problems/math-0002-hypercube-isolation/formal/lakefile.lean
@@ -7,4 +7,5 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.1"
 
 @[default_target]
-lean_lib HypercubeIsolation
+lean_lib HypercubeIsolation where
+  roots := #[`HypercubeIsolation, `HypercubeIsolation.Puncturing]
